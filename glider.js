99a86831fc5b7face1fbd050526ca239f3920e9d
3mkrass <149709889+3mkrass@users.noreply.github.com>
2024-04-04 17:01:46 +0100

    Add files via upload

---
+++ glider.js
@@ -0,0 +1,5 @@
+<!-- Glider.js CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/glider-js/1.7.7/glider.min.css">
+
+<!-- Glider.js JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/glider-js/1.7.7/glider.min.js"></script>
